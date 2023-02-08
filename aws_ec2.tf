1446e1a6a0d352090174f4430cb798f826d92277
Sitabja Chatterjee <89836369+sitchatt@users.noreply.github.com>
2023-02-08 21:41:11 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -8,7 +8,6 @@ resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   count = 2
-  instance_type = "t2.micro"
 
   tags = {
     Name = "example-instance"
